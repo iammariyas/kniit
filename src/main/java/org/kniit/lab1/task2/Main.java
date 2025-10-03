ed99fbb88a5a2dde5e968859b7a56faefae2ff1f
Maria <mariakruleva@gmail.com>
2025-10-03 23:42:36 +0400

    Задача 2

--- src/main/java/org/kniit/lab1/task2/Main.java
+++ src/main/java/org/kniit/lab1/task2/Main.java
@@ -1,4 +1,10 @@
 package org.kniit.lab1.task2;
 
 public class Main {
+    public static void main(String[] args) {
+        String url = "https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText";
+
+        SimpleURL simpleURL = new SimpleURL(url);
+        System.out.println(simpleURL.toString());
+    }
 }
---
+++ src/main/java/org/kniit/lab1/task2/SimpleURL.java
@@ -0,0 +1,122 @@
+package org.kniit.lab1.task2;
+
+/* Ссылка из условия:
+https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText */
+public class SimpleURL {
+    private String protocol;
+    private String host;
+    private String path;
+    private int intParam;
+    private double doubleParam;
+    private String textParameter;
+
+
+    public SimpleURL(String url) {
+        parseUrl(url);
+    }
+
+
+    private void parseUrl(String url) {
+        if (url.startsWith("https://")) {
+            this.protocol = "https";
+            url = url.substring(8);
+        }
+
+        int indexSlash = url.indexOf('/');
+        if (indexSlash != -1) {
+            this.host = url.substring(0, indexSlash);
+            this.path = url.substring(indexSlash);
+        }
+
+        if (url.contains("intParam=")) {
+            int start = url.indexOf("intParam=") + 9;
+            int end = url.indexOf("&", start);
+            if (end == -1) {
+                end = url.length();
+            }
+
+            String number = url.substring(start, end);
+            this.intParam = Integer.parseInt(number);
+        }
+
+        if (url.contains("doubleParam=")) {
+            int start = url.indexOf("doubleParam=") + 12;
+            int end = url.indexOf("&", start);
+            if (end == -1) {
+                end = url.length();
+            }
+
+            String number = url.substring(start, end);
+            this.doubleParam = Double.parseDouble(number);
+        }
+
+        if (url.contains("textParameter=")) {
+            int start = url.indexOf("textParameter=") + 14; //
+            int end = url.indexOf("&", start);
+            if (end == -1) {
+                end = url.length(); //
+            }
+
+            this.textParameter = url.substring(start, end);
+        }
+    }
+
+    // Геттеры:
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public int getIntParam() {
+        return intParam;
+    }
+
+    public double getDoubleParam() {
+        return doubleParam;
+    }
+
+    public String getTextParameter() {
+        return textParameter;
+    }
+
+    // Сеттеры:
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setIntParam(int intParam) {
+        this.intParam = intParam;
+    }
+
+    public void setDoubleParam(double doubleParam) {
+        this.doubleParam = doubleParam;
+    }
+
+    public void setTextParameter(String textParameter) {
+        this.textParameter = textParameter;
+    }
+
+    // Переопределим метод toString():
+    @Override
+    public String toString() {
+        return String.format("protocol = %s\nhost = %s\npath = %s\n" +
+                "intParam = %s\ndoubleParam = %s\ntextParameter = %s", "param", "host", "path",
+                intParam, doubleParam, "textParameter");
+    }
+}
+
