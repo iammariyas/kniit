9e291e7ed7d1dc9c32f1a4ad62b7af3bf0203cd7
Maria Kruleva <mariakruleva@gmail.com>
2025-12-05 21:31:29 +0400

    task 18

---
+++ src/main/java/org/kniit/lab10/task18/Main.java
@@ -0,0 +1,16 @@
+package org.kniit.lab10.task18;
+
+public class Main {
+    static public void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("Age", 30);
+        System.out.println(pair.getFirst()); // Age
+        System.out.println(pair.getSecond()); // 30
+
+        pair.setSecond(35);
+        System.out.println(pair.getSecond()); // 35
+
+        pair.setFirst("Количество");
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+    }
+}
